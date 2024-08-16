4568b6be82d5381acbed92536b012d92507b1676
RonB8 <robuaron8@gmail.com>
2024-08-16 10:52:02 +0300

    /

--- q2.cpp
+++ q2.cpp
@@ -137,7 +137,7 @@ Handler hand;
 	std::cout << (void*)funcPtr << std::endl;
 	std::cout << (void*)funcPtr11 << std::endl;
 	std::cout << (void*)funcPtr1 << std::endl;
-	(void*)funcPtr1();
+	(hand.*(funcPtr1))();
 	exit(0);
 
 
