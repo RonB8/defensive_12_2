bbf26158a51a083d71e9e620d71b97f04311080b
RonB8 <robuaron8@gmail.com>
2024-08-16 10:38:58 +0300

    /

--- q2.cpp
+++ q2.cpp
@@ -130,7 +130,7 @@ Handler hand;
 	void (Handler::*funcPtr)(const char*) = &Handler::interpret;
 	void (Handler::*funcPtr1)() = &Handler::print_adresses;
 	uintptr_t a1 = reinterpret_cast<uintptr_t&>(funcPtr1);
-	a1 += 26;
+	a1 += 1;
 	void (*funcPtr11)() =  reinterpret_cast<void (*)()>(a1);
 	std::cout << (void*)funcPtr << std::endl;
 	std::cout << (void*)funcPtr11 << std::endl;
